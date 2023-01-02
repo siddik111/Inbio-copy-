57e66abd95b8bd28de8ed81382e0c74b5f254758
siddik111 <mdsiddikdpi@gmail.com>
2023-01-03 00:29:09 +0600

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,34 @@
+
+
+
+
+var time='10am';
+
+if(time == "8am"){
+console.log("ghum theke uthija babu");
+}
+else if(time == '9am'){
+    console.log('go to school')
+
+}
+else if (time == '11am'){
+    console.log('gosol kor joldi')
+}
+else if (time == '10am'){
+    console.log('gosol na koris akhono')
+}
+else if (time == '120am'){
+    console.log('kibe ')
+}
+else if (time == '1pm'){
+    console.log('ja be kaj kor')
+}
+else if (time == '2pm'){
+    console.log('raiter ta khaya ghumi ja re ')
+}
+else if (time == '3pm'){
+    console.log('khaw ki sesh')
+}
+else if (time == '4pmm'){
+    console.log('ghuma akhon tahole')
+}
